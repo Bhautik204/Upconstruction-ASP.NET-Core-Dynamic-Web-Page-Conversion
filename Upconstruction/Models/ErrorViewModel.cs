cf88f9f486fbd782271169aa4e458a76ddac57df
Bhautik204 <118058581+Bhautik204@users.noreply.github.com>
2024-08-16 12:53:14 +0530

    initial commit

---
+++ Upconstruction/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Upconstruction.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
